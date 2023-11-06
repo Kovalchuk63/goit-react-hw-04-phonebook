a7e400b9f501929db48ba84e71b490f813c4a74c
Kovalchuk63 <128521528+Kovalchuk63@users.noreply.github.com>
2023-11-06 22:14:43 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,89 @@
+import React from 'react';
+import { nanoid } from 'nanoid';
+import { Phonebook } from 'components/Phonebook/Phonebook';
+import { Filter } from 'components/Filter/Filter';
+import { ContactsList } from 'components/Contacts/Contacts';
+import { Layout, TitleH1, TitleH2 } from 'Layout';
+
+export class App extends React.Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  componentDidMount() {
+    const contactsLs = localStorage.getItem('contacts');
+    if (contactsLs !== null) {
+      this.setState({
+        contacts: JSON.parse(contactsLs),
+      });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  addContact = newContact => {
+    const isContactExists = this.state.contacts.some(
+      contact => contact.name === newContact.name
+    );
+
+    if (isContactExists) {
+      alert(`Контакт з іменем ${newContact.name} вже існує.`);
+      return;
+    }
+
+    this.setState(prevState => {
+      return {
+        contacts: [...prevState.contacts, { ...newContact, id: nanoid() }],
+      };
+    });
+  };
+
+  deleteContact = id => {
+    this.setState(prevState => {
+      return {
+        contacts: prevState.contacts.filter(contact => contact.id !== id),
+      };
+    });
+  };
+
+  filter = target => {
+    this.setState({
+      filter: target,
+    });
+  };
+
+  visibleItems = () => {
+    const { contacts, filter } = this.state;
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  };
+
+  render() {
+    const { contacts, filter } = this.state;
+    const visibleItems = this.visibleItems();
+    return (
+      <React.Fragment>
+        <Layout>
+          <TitleH1>Phonebook</TitleH1>
+          <Phonebook onAddContact={this.addContact} />
+        </Layout>
+        {contacts.length > 0 && (
+          <Layout>
+            <TitleH2>Contacts</TitleH2>
+            <Filter inputValue={filter} onFilter={this.filter} />
+            <ContactsList
+              contacts={visibleItems}
+              onDeleteContact={this.deleteContact}
+            />
+          </Layout>
+        )}
+      </React.Fragment>
+    );
+  }
+}
